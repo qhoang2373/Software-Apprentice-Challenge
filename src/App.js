e770d761e404d8d241ad3af7d3bf278de8d05ac3
Quan <qhoang2373@gmail.com>
2025-06-04 17:36:12 -0700

    complete hooks components and fetch data

--- src/App.js
+++ src/App.js
@@ -6,41 +6,40 @@ import React, { useState, useEffect } from 'react'; // importing react hooks
 
 function App() {
   const [ads, setAds] = useState([]); // will hold fetched data
+  
 
-useEffect(() => { // tells react to fetch data 
-  const fetchAds = async () => {
-    try {
-      const response = await fetch('http://localhost:3000/fakeDataSet'); // data will be fetched from fakeDataSet
-      if (!response.ok) { // Checks if the response was successful 
+useEffect(() => { // tells react to fetch data
+  fetch('http://localhost:3000/fakeDataSet') // data will be fetched from the fakeDataSet API
+    .then(response => {
+      if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`); // if not, then throw error
       }
-      const data = await response.json();
-
-      setAds(data); // update the ads with fetched data
-      // Set 'loading' state to false, indicating data fetching is complete
+      return response.json();
+    })
+    .then(data => {
+      setAds(data); 
       setLoading(false);
-    } catch (error) {
+    })
+    .catch(error => {
       setError(error);
       setLoading(false);
-      console.error("Error fetching data:", error);
-    }
-  };
-  fetchAds();
+      console.error("Error fetching data:", error); // logs error message to console
+    });
 }, []); 
 
-
 return (
-  <div className="App">  
-    <h1>Ad Dashboard</h1> {/* Heading of the page */}
-    {loading && <p>Loading ads...</p>} {/* Conditional rendering, if left is true, right will be true */}
-    {error && <p>Error: {error.message}</p>}  
+  <div className="App">
+    <h1>Ad Dashboard</h1>  {/* Main heading of the page */}
+    {loading && <p>Loading ads...</p>}  {/* Conditional rendering, if left is true then, right will render  */}
+    {error && <p>Error: {error.message}</p>}
     <ul>
       {ads.map((ad, index) => ( 
         <li key={index}>{ad.campaign_name || ad.utm_campaign || 'No Campaign Name'}</li> 
-      ))} {/* Iterates over Ad array  */}
+      ))}  {/* .map iterates over the ads array  */}
     </ul>
   </div>
 );
 }
 
 export default App;
+
